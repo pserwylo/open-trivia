8c29005d2fe8af27bfdc2ba0f4c8e7925958758e
Peter Serwylo <peter@ivt.com.au>
2013-01-05 20:16:37 +1100

    Fixed toggles so that they have show/hide listeners to correctly hide individual items when we hide all items.

--- web/web-app/js/answerSheet.js
+++ web/web-app/js/answerSheet.js
@@ -2,17 +2,34 @@ $( function() {
 
 	$( ".toggle-answers" ).each( function( i, item ) {
 
+		var individualAnswerToggles = [];
 		var btn = $( item );
 		var answers = btn.closest( '.round' ).find( '.answer' );
-		new Utils().setupToggle( btn, answers.find( '.value' ).hide(), answers.find( '.show' ) );
 
 		answers.each( function( j, answer) {
 
 			var answerBtn = $( answer ).find( '.show' );
 			var answerToggle = $( answer ).find( '.value' );
-			new Utils().setupToggle( answerBtn, answerToggle );
+			individualAnswerToggles.push( new Utils.Toggle( answerBtn, answerToggle ) );
 
 		});
+
+		var toggleAll = new Utils.Toggle(
+			btn,
+			answers.find( '.value' ).hide(),
+			{
+				invertedToggleItems: answers.find( '.show' ),
+				onHide: function() {
+
+					// Ensures that when we: toggle an individual to show, then toggling all to show, then hiding all,
+					// the individual which we showed before is hidden (because we hid all of them).
+					$( individualAnswerToggles ).each( function( i, item ) {
+						item.hide();
+					});
+
+				}
+			}
+		);
 
 	});
 
--- web/web-app/js/utils.js
+++ web/web-app/js/utils.js
@@ -1,38 +1,82 @@
-var Utils = klass( function() {
+var Utils = {};
+
+/**
+ * @param button jQuery object which references the button used to toggle the toggleItems.
+ * @param toggleItems jQuery object which references all of the items to be toggled on or off.
+ * @param invertedToggleItems Does the reverse, shows when it should hide and hide when it should show.
+ */
+Utils.Toggle = klass( function( button, toggleItems, options ) {
+
+	this.button = button;
+	this.toggleItems = toggleItems;
+	this.invertedToggleItems = null;
+	this.onShow = null;
+	this.onHide = null;
+
+	if ( typeof options !== "undefined" && options != null ) {
+
+		if ( options.hasOwnProperty( 'invertedToggleItems' ) ) {
+			this.invertedToggleItems = options.invertedToggleItems;
+		}
+
+		if ( options.hasOwnProperty( 'onShow' ) ) {
+			this.onShow = options.onShow;
+		}
+
+		if ( options.hasOwnProperty( 'onHide' ) ) {
+			this.onHide = options.onHide;
+		}
+
+	}
+
+	var self = this;
+
+	this.button.click( function() { self.onClick() } );
 
 }).statics({
 
 }).methods({
 
-	/**
-	 * @param button jQuery object which references the button used to toggle the toggleItems.
-	 * @param toggleItems jQuery object which references all of the items to be toggled on or off.
-	 * @param invertedToggleItems Does the reverse, shows when it should hide and hide when it should show.
-	 */
-	setupToggle: function( button, toggleItems, invertedToggleItems ) {
+	onClick: function() {
 
-		button.click( function() {
+		if ( this.button.html().substring( 0, 4 ) == 'Hide' )
+		{
+			this.hide();
+		}
+		else
+		{
+			this.show();
+		}
 
-			if ( this.innerHTML.substring( 0, 4 ) == 'Hide' )
-			{
-				this.innerHTML = this.innerHTML.replace( 'Hide', 'Show' );
-				toggleItems.hide( 'fast' );
+	},
 
-				if ( typeof invertedToggleItems !== "undefined" ) {
-					invertedToggleItems.show( 'fast' );
-				}
-			}
-			else
-			{
-				this.innerHTML = this.innerHTML.replace( 'Show', 'Hide' );
-				toggleItems.show( 'fast' );
+	show: function() {
 
-				if ( typeof invertedToggleItems !== "undefined" ) {
-					invertedToggleItems.hide( 'fast' );
-				}
-			}
+		this.button.html( this.button.html().replace( 'Show', 'Hide' ) );
+		this.toggleItems.show();
 
-		});
+		if ( this.invertedToggleItems != null ) {
+			this.invertedToggleItems.hide();
+		}
+
+		if ( this.onShow != null ) {
+			this.onShow();
+		}
+
+	},
+
+	hide: function() {
+
+		this.button.html( this.button.html().replace( 'Hide', 'Show' ) );
+		this.toggleItems.hide();
+
+		if ( this.invertedToggleItems != null ) {
+			this.invertedToggleItems.show();
+		}
+
+		if ( this.onHide != null ) {
+			this.onHide();
+		}
 
 	}
 
