9fc412b6dcf7765e6bf64edeb527e19c05d8b2c3
Peter Serwylo <peter@ivt.com.au>
2013-07-29 15:56:00 +1000

    Default users to enabled. Fixes issue #4.

--- web/grails-app/domain/com/serwylo/trivia/auth/User.groovy
+++ web/grails-app/domain/com/serwylo/trivia/auth/User.groovy
@@ -7,10 +7,10 @@ class User {
 	String username
 	String password
 	String salt
-	boolean enabled
-	boolean accountExpired
-	boolean accountLocked
-	boolean passwordExpired
+	boolean enabled         = true
+	boolean accountExpired  = false
+	boolean accountLocked   = false
+	boolean passwordExpired = false
 
 	static constraints = {
 		username blank: false, unique: true
